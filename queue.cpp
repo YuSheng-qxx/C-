d3ec55b580caa38f538c0ae0928c0c1c2f86f36c
YuSheng-qxx <62830655+YuSheng-qxx@users.noreply.github.com>
2020-11-21 14:02:51 +0800

    提交代码

---
+++ queue.cpp
@@ -0,0 +1,56 @@
+//#include<queue>
+//#include<list>
+//#include<iostream>
+//using namespace std;
+//
+//template <class T, class container>
+//class Queue
+//{
+//public:
+//	void push(const T& val)
+//	{
+//		_lst.push_back(val);
+//	}
+//
+//	void pop()
+//	{
+//		_lst.pop_front();
+//	}
+//
+//	T& front()
+//	{
+//		return _lst.front();
+//	}
+//
+//	bool empty()
+//	{
+//		return _lst.empty();
+//	}
+//
+//	T& back()
+//	{
+//		return _lst.back();
+//	}
+//
+//	size_t size()
+//	{
+//		return _lst.size();
+//	}
+//private:
+//	container _lst;
+//};
+
+//void test()
+//{
+//	queue<int> q;
+//	q.push(1);
+//	q.push(2);
+//	q.push(3);
+//	q.push(4);
+//	while (!q.empty())
+//	{
+//		cout << q.front() << " ";
+//		q.pop();
+//	}
+//	cout << endl;
+//}
---
+++ stack.cpp
@@ -0,0 +1,116 @@
+//#include<stack>
+//#include<vector>
+//using namespace std;
+//#include<iostream>
+//
+//template <class T, class container>
+//class Stack
+//{
+//public:
+//	void push(const T& val)
+//	{
+//		_v.push_back(val);
+//	}
+//
+//	void pop()
+//	{
+//		_v.pop_back();
+//	}
+//
+//	T& top()
+//	{
+//		return _v.back();
+//	}
+//
+//	bool empty()
+//	{
+//		return _v.empty();
+//	}
+//
+//	size_t size()
+//	{
+//		return _v.size();
+//	}
+//
+//private:
+//	container _v;
+//};
+//
+//void test()
+//{
+//	Stack<int> st;
+//	st.push(1);
+//	st.push(2);
+//	st.push(3);
+//	st.push(4);
+//	while (!st.empty())
+//	{
+//		cout << st.top() << " ";
+//		st.pop();
+//	}
+//	cout << endl;
+//}
+//
+//int main()
+//{
+//	test();
+//	system("pause");
+//	return 0;
+//}
+
+
+
+
+//最小栈
+//class MinStack
+//{
+//	stack<int> _stack;
+//	stack<int> _minstack;
+//	void push(int x)
+//	{
+//		_stack.push(x);
+//		if (_minstack.empty() || x <= _minstack.top())
+//		{
+//			_minstack.push(x);
+//		}
+//	}
+//
+//	void pop()
+//	{
+//		if (!_minstack.empty() && _stack.top() == _minstack.top())
+//		{
+//			_minstack.pop();
+//		}
+//		_stack.pop();
+//	}
+//
+//	int top()
+//	{
+//		return _stack.top();
+//	}
+//
+//	int getMin()
+//	{
+//		return _minstack.top();
+//	}
+//};
+
+
+
+//栈接口测试
+//void test()
+//{
+//	stack<int> st;
+//	st.push(1);
+//	st.push(2);
+//	st.push(3);
+//	st.push(4);
+//	st.push(5);
+//
+//	while (!st.empty())
+//	{
+//		cout << st.top() << " ";
+//		st.pop();
+//	}
+//	cout << endl;
+//}
